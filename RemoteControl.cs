2dc43a61d365bee6f0021731110bc6efeaf43013
jim berry <jim@bigfattail.com>
2020-01-07 16:16:45 -0500

    Control module for remote bikes

---
+++ RemoteControl.cs
@@ -0,0 +1,17 @@
+﻿using BeamBackend;
+
+namespace BikeControl
+{
+    public class RemoteControl : BikeControlBase
+    {
+        public override void SetupImpl() 
+        {
+            
+        }
+
+        public override void Loop(float frameSecs)
+        {
+        }  
+
+    }
+}
